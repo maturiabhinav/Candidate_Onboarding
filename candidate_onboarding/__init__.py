1044cf4800234d30ea67ffb76ba8c35746c5fa53
maturiabhinav <abhinav.m@futureinvo.com>
2025-11-29 13:35:35 +0530

    no files

--- candidate_onboarding/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_mail import Mail
-import os
-
-db = SQLAlchemy()
-login_manager = LoginManager()
-mail = Mail()
